f2fb1d59f8209ae0c08a12deb234dc8f3fea1e6a
CheoljongPaek <cjpaek0122@gmail.com>
2021-06-14 11:39:33 +0900

    Comment to Chapter2 Webpack

--- about-webpack/ch2/webpack.config.js
+++ about-webpack/ch2/webpack.config.js
@@ -2,12 +2,18 @@ var path = require('path');
 
 module.exports = {
 	mode: 'production',
+	/*
+		Webpack starts building a relation of modules usde in a web app through the entry file, so the file should contain overall contents. 
+		entry point can be more than one.
+	*/
 	entry: './js/app.js',
 	output: {
 			path: path.resolve(__dirname, 'build'),
 			filename: 'main.bundle.js'
 	},
+	/* entry -> (module: loader for various sources) -> output */
 	module: {
+		/* rules: each object is a loader setting for files having specific ext */
 		rules: [{
 			test: /\.m?js$/,
 			exclude: /(node_modules|bower_components)/,
